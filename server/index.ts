70c1298c3d75b84bc6ccc5d781cf53069cdb247f
Idris Akintobi Adejare <akintobiidris@gmail.com>
2025-11-29 22:48:02 +0100

    chore: added graceful shutdown function

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
 import app from "./app.js";
 import { config } from "./config/env.js";
-import { connectDB } from "./db/connection.js";
+import { connectDB, Database } from "./db/connection.js";
 import { logger } from "./utils/index.js";
 
-// Start Server
-if (config.nodeEnv !== "test") {
+let server: any;
+const SHUTDOWN_TIMEOUT_MS = 10_000;
+
+async function startServer() {
   try {
     await connectDB();
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Server running on port ${config.port}`);
     });
   } catch (error) {
@@ -15,3 +17,44 @@ if (config.nodeEnv !== "test") {
     process.exit(1);
   }
 }
+
+async function gracefulShutdown(signal: string) {
+  logger.info(`${signal} received, starting graceful shutdown`);
+
+  if (server) {
+    server.close(async () => {
+      logger.info("HTTP server closed");
+
+      try {
+        const db = Database.getInstance();
+        await db.disconnect();
+        process.exit(0);
+      } catch (error) {
+        logger.error("Error during shutdown", error);
+        process.exit(1);
+      }
+    });
+
+    setTimeout(() => {
+      logger.error("Forced shutdown after timeout");
+      process.exit(1);
+    }, SHUTDOWN_TIMEOUT_MS);
+  } else {
+    process.exit(0);
+  }
+}
+
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+
+process.on("uncaughtException", (error: Error) => {
+  logger.fatal("Uncaught Exception", error);
+  gracefulShutdown("uncaughtException");
+});
+
+process.on("unhandledRejection", (reason: any) => {
+  logger.fatal("Unhandled Rejection", reason);
+  gracefulShutdown("unhandledRejection");
+});
+
+startServer();
